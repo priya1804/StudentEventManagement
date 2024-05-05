56fb6f803ad5493851c176f0b43521171dca6725
priya1804 <priyu184@gmail.com>
2024-05-06 01:34:07 +0530

    changes for logged in user or not

--- admin/listofevents/upcomingapproved.php
+++ admin/listofevents/upcomingapproved.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+$mysqli = new mysqli("localhost", "root", "", "student_event_manage");
+
+if (!isset($_SESSION['user_id'])) {
+    header("location:../../login.php");
+}
+?>
+
 <!doctype html>
 <html lang="en">
 
