c0d766025801a0bae7c785b16f0a7292aa64814e
priya1804 <priyu184@gmail.com>
2024-05-07 20:44:49 +0530

    Setup for email

--- sendEmails.php
+++ sendEmails.php
@@ -14,8 +14,8 @@ function sendMailForEventReg($studentsEmailArray){
     // SMTP configuration
     $mail->Host = 'smtp.gmail.com'; // Specify main and backup SMTP servers
     $mail->SMTPAuth = true; // Enable SMTP authentication
-    $mail->Username = 'priyashukla18@gmail.com'; // SMTP username
-    $mail->Password = 'cakwnonqnwgsaaia'; // SMTP password
+    $mail->Username = 'studenteventmanagement2024@gmail.com'; // SMTP username
+    $mail->Password = 'nkxiwqiklyebwdgk'; // SMTP password
     $mail->SMTPSecure = 'tls'; // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587; // TCP port to connect to
     
